8831d31675bc6a3b7314f77b3f943d64a0c98eb9
Rahmat Kurniawan <rahmatkurniawan811@gmail.com>
2019-12-06 04:17:38 +0700

    [rahmatkurniawan] ModelLeader - add leader model

---
+++ app/src/main/java/co/id/iconpln/listleadersapp/ModelLeader.kt
@@ -0,0 +1,8 @@
+package co.id.iconpln.listleadersapp
+
+data class ModelLeader (
+    var name: String = "",
+    var desc: String = "",
+    var country: String = "",
+    var photo: String = ""
+)
