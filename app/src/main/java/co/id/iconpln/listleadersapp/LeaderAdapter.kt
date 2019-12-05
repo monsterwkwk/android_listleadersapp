6b26714da1ee0030cd49bd2095e5b40d6ff08caf
Rahmat Kurniawan <rahmatkurniawan811@gmail.com>
2019-12-06 04:30:04 +0700

    [rahmatkurniawan] LeadersAdapter - Implement method interface

--- app/src/main/java/co/id/iconpln/listleadersapp/LeaderAdapter.kt
+++ app/src/main/java/co/id/iconpln/listleadersapp/LeaderAdapter.kt
@@ -1,24 +1,25 @@
 package co.id.iconpln.listleadersapp
 
+import android.content.Context
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
 
-class LeaderAdapter: BaseAdapter() {
+class LeaderAdapter(val context: Context, val listWorldLeaders: ArrayList<DataLeader>): BaseAdapter() {
     override fun getView(index: Int, view: View?, viewGroup: ViewGroup?): View {
 
     }
 
     override fun getItem(index: Int): Any {
-
+        return listWorldLeaders[index]
     }
 
     override fun getItemId(index: Int): Long {
-
+        return index.toLong()
     }
 
     override fun getCount(): Int {
-
+        return listWorldLeaders.size
     }
 
 }
