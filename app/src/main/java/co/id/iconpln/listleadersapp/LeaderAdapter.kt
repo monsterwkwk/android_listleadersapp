dd6a29558811229a5014a8d6d8f468ed8c7ed0f7
Rahmat Kurniawan <rahmatkurniawan811@gmail.com>
2019-12-06 04:27:13 +0700

    [rahmatkurniawan] LeaderAdapter - add base adapter

---
+++ app/src/main/java/co/id/iconpln/listleadersapp/LeaderAdapter.kt
@@ -0,0 +1,24 @@
+package co.id.iconpln.listleadersapp
+
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+
+class LeaderAdapter: BaseAdapter() {
+    override fun getView(index: Int, view: View?, viewGroup: ViewGroup?): View {
+
+    }
+
+    override fun getItem(index: Int): Any {
+
+    }
+
+    override fun getItemId(index: Int): Long {
+
+    }
+
+    override fun getCount(): Int {
+
+    }
+
+}
