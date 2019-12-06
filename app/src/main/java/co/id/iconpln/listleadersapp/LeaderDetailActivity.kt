fcd2bf39774382b0d8c9f5e240166036f2293073
Rahmat Kurniawan <rahmatkurniawan811@gmail.com>
2019-12-06 13:09:59 +0700

    [rahmatkurniawan] LeaderDetail - add companion object and lateinit variable

--- app/src/main/java/co/id/iconpln/listleadersapp/LeaderDetailActivity.kt
+++ app/src/main/java/co/id/iconpln/listleadersapp/LeaderDetailActivity.kt
@@ -5,6 +5,17 @@ import android.os.Bundle
 
 class LeaderDetailActivity : AppCompatActivity() {
 
+    companion object {
+        const val EXTRA_NAME = "extra_name"
+        const val EXTRA_DESC = "extra_desc"
+        const val EXTRA_COUNTRY = "extra_country"
+        const val EXTRA_IMAGE_URL = "extra_image_url"
+
+        const val EXTRA_HERO = "extra_hero"
+    }
+
+    private lateinit var leader: ModelLeader
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_leader_detail)
